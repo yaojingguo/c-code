b4e4546a76539604fe041a64242ff1fb22d3cc61
Jingguo Yao <yaojingguo@gmail.com>
2012-06-14 16:44:13 +0800

    Add

---
+++ perror.c
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(int argc, const char *argv[]) {
+	int fd;
+	char *filename = "nonexistent";
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0) {
+		perror("open error");
+	}
+	
+	return 0;
+}
