ff93c806063f98c7db0657b1b2cdaa41b8d884fa
Jingguo Yao <yaojingguo@gmail.com>
2012-06-14 16:50:41 +0800

    Add

---
+++ htonl.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+
+int main(int argc, const char *argv[]) {
+	int h, l;
+
+	h = 0x11223344;
+	l = htonl(h);
+	printf("host byte order: 0X%8x\n", h);			// little endian on Linux x86
+	printf("network byte order: 0X%8x\n", l);   // big endian
+
+	return 0;
+}
