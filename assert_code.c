fe2a4bef6e87842291ce2492bf820ad1fd4efae9
Jingguo Yao <yaojingguo@gmail.com>
2012-06-27 09:40:55 +0800

    Add

---
+++ assert_code.c
@@ -0,0 +1,7 @@
+// #define NDEBUG
+#include <assert.h>
+
+int main(int argc, const char *argv[]) {
+	assert(0 == 1);	
+	return 0;
+}
---
+++ typedef.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+typedef int INT;
+
+int main(int argc, const char *argv[]) {
+	INT i;
+	i = 10;
+	printf("i: %d\n", i);
+	return 0;
+}
