c4cc0ed1c838a636acd753464827496df54b1917
Jingguo Yao <yaojingguo@gmail.com>
2012-07-11 16:31:56 +0800

    Add

---
+++ empty-null.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[]) 
+{
+  char* p = "";
+  if (p == 0)
+    printf("wrong\n");
+  else
+    printf("right\n");
+  return 0;
+}
---
+++ open.c
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char *argv[]) 
+{
+  int fd;
+  fd = open("new-file", O_RDWR | O_CREAT | O_TRUNC, S_IRWXU);
+  if (fd == -1) {
+    perror(NULL);
+    exit(1);
+  }
+
+  char *buf = "abc";
+  size_t count = 1;
+  int written;
+  written = write(fd, buf, count);
+
+  if (written != -1)
+    printf("written: %d\n", written);
+
+  close(fd);
+  return 0;
+}
