273c2b599d8ef5ed64a01589cf0fe95f3de1e747
Jingguo Yao <yaojingguo@gmail.com>
2012-12-03 22:59:50 +0800

    Add

--- initializer.c
+++ initializer.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 // See discussion about fewer initializers at:
 // http://stackoverflow.com/questions/1352370/c-static-array-initialization-how-verbose-do-i-need-to-be
-void func()
+void test1()
 {
   int array[10] = {};
   int i;
@@ -10,11 +10,17 @@ void func()
     printf("array[%d] = %d\n", i, array[i]);
 }
 
-int main(int argc, const char *argv[]) 
+void test2()
 {
-  func();
-  return 0;
+  int i;
+  int array[2] = { [0] = 10, [1] = 100 };
+  for (i = 0; i < 2; i++)
+    printf("array[%d] = %d\n", i, array[i]);
 }
 
-
-
+int main(int argc, const char *argv[]) 
+{
+  test1();
+  test2();
+  return 0;
+}
