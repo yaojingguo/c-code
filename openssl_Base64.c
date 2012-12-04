735a637a81c68bccb97bc178ccb1c230b5b0cf23
Jingguo Yao <yaojingguo@gmail.com>
2012-12-04 13:34:59 +0800

    Reviewed

--- openssl_Base64.c
+++ openssl_Base64.c
@@ -5,29 +5,32 @@
 #include <openssl/bio.h>
 #include <openssl/buffer.h>
 
-char *unbase64(unsigned char *input, int length);
+char *base64(const unsigned char *input, int length);
 
 int main(int argc, char **argv)
 {
-  char *output = unbase64("WU9ZTyEA\n\0", strlen("WU9ZTyEA\n\0"));
-  printf("Unbase64: *%s*\n", output);
+  char *output = base64("YOYO!", sizeof("YOYO!"));
+  printf("Base64: >%s<\n", output);
   free(output);
 }
 
-char *unbase64(unsigned char *input, int length)
+char *base64(const unsigned char *input, int length)
 {
-  BIO *b64, *bmem;
-
-  char *buffer = (char *)malloc(length);
-  memset(buffer, 0, length);
+  BIO *bmem, *b64;
+  BUF_MEM *bptr;
 
   b64 = BIO_new(BIO_f_base64());
-  bmem = BIO_new_mem_buf(input, length);
-  bmem = BIO_push(b64, bmem);
+  bmem = BIO_new(BIO_s_mem());
+  b64 = BIO_push(b64, bmem);
+  BIO_write(b64, input, length);
+  BIO_flush(b64);
+  BIO_get_mem_ptr(b64, &bptr);
 
-  BIO_read(bmem, buffer, length);
+  char *buff = (char *)malloc(bptr->length);
+  memcpy(buff, bptr->data, bptr->length-1);
+  buff[bptr->length-1] = 0;
 
-  BIO_free_all(bmem);
+  BIO_free_all(b64);
 
-  return buffer;
+  return buff;
 }
