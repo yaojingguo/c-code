26e72c27fb9035526cd003bb406419906958b864
Jingguo Yao <yaojingguo@gmail.com>
2012-12-04 14:33:23 +0800

    Improvement

--- initializer.c
+++ initializer.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
 // See discussion about fewer initializers at:
 // http://stackoverflow.com/questions/1352370/c-static-array-initialization-how-verbose-do-i-need-to-be
-void test1()
-{
-  int array[10] = {};
-  int i;
 
-  for (i = 0; i < 10; i++)
-    printf("array[%d] = %d\n", i, array[i]);
+void info(int arr[], int len, char* name)
+{
+  int i;
+  printf("======= name\n", name);
+  for (i = 0; i < len; i++)
+    printf("array[%d] = %d\n", i, arr[i]);
 }
 
-void test2()
+void test()
 {
   int i;
-  int array[2] = { [0] = 10, [1] = 100 };
-  for (i = 0; i < 2; i++)
-    printf("array[%d] = %d\n", i, array[i]);
+
+  int first[5];
+  info(first, 5, "first");
+
+  int second[5] = {};
+  info(second, 5, "second");
+
+  int third[5] = {0};
+  info(third, 5, "third");
+
+  int fourth[5] = { 1 };
+  info(fourth, 5, "fourth");
+
+  int fifth[5] = { [0] = 10, [2] = 100, };
+  info(fifth, 5, "fifth");
 }
 
 int main(int argc, const char *argv[]) 
 {
-  test1();
-  test2();
+  test();
   return 0;
 }
