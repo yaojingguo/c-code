c924b6951ff7cca7104858d1d8c0d602532196bc
Jingguo Yao <yaojingguo@gmail.com>
2012-12-10 14:52:45 +0800

    Tested

--- digest/sha1/sha1.c
+++ digest/sha1/sha1.c
@@ -7,25 +7,6 @@
 #define S(bits,word) \
   (((word) << (bits)) | ((word) >> (32-(bits))))
 
-char table[] = {
-  [0]  = '0',
-  [1]  = '1',
-  [2]  = '2',
-  [3]  = '3',
-  [4]  = '4',
-  [5]  = '5',
-  [6]  = '6',
-  [7]  = '7',
-  [8]  = '8',
-  [9]  = '9',
-  [10] = 'a',
-  [11] = 'b',
-  [12] = 'c',
-  [13] = 'd',
-  [14] = 'e',
-  [15] = 'f'
-};
-
 uint32_t f(int t, uint32_t B, uint32_t C, uint32_t D) 
 {
   if (0 <= t && t <= 19)
@@ -68,21 +49,14 @@ uint8_t* pad(uint8_t* in, int inl, uint64_t l, uint8_t* block)
     block[i] = p[7-i];
 }
 
-// l: byte length
-uint8_t* sha1(uint8_t *message, uint64_t l, uint8_t* digest)
+// block: 64 bytes
+// digest: 20 bytes
+void process_block(uint8_t* block, uint8_t* digest)
 {
-  // pad
-  uint64_t nblock, i;
-  int nremainder, t;
-  uint8_t last[64];
-  uint8_t W[80];
+  uint32_t W[80];
   uint32_t temp, A, B, C, D, E;
   uint32_t* H;
-
-  nblock = l / 64;
-  nremainder = l % 64;
-  if (nremainder > 0)
-    pad(message + nblock * 64, nremainder, l, last);
+  int i, t;
 
   H = (uint32_t*) digest;
   H[0] = 0x67452301;
@@ -91,55 +65,84 @@ uint8_t* sha1(uint8_t *message, uint64_t l, uint8_t* digest)
   H[3] = 0x10325476;
   H[4] = 0xC3D2E1F0;
 
-  for (i = 0; i <= nblock; ++i) {
-    if (i < nblock)
-      memcpy(W, message +  64 * i, 64);
-    else if (nremainder > 0)
-      memcpy(W, last, 64);
-    else
-      break;
-    for (t = 16; t <= 79; ++t) 
-      W[t] = S(1, (W[t-3] ^ W[t-8] ^ W[t-14] ^ W[t-16]));
-    A = H[0]; 
-    B = H[1]; 
-    C = H[2]; 
-    D = H[3]; 
-    E = H[4];
-    for (t = 0; t <= 79; ++t) {
-      temp = S(5, A) + f(t, B, C, D) + E + W[t] + K(t);
-      E = D;
-      D = C;
-      C = S(30, B);
-      B = A;
-      A = temp;
-    }
-    H[0] += A;
-    H[1] += B;
-    H[2] += C;
-    H[3] += D;
-    H[4] += E;
+  for (t = 0; t <= 15; ++t) {
+    W[t] = block[4 * t] << 24;
+    W[t] |= block[4 * t + 1] << 16;
+    W[t] |= block[4 * t + 2] << 8;
+    W[t] |= block[4 * t + 3];
   }
+
+  for (t = 16; t <= 79; ++t) 
+    W[t] = S(1, (W[t-3] ^ W[t-8] ^ W[t-14] ^ W[t-16]));
+
+  A = H[0]; 
+  B = H[1]; 
+  C = H[2]; 
+  D = H[3]; 
+  E = H[4];
+
+  printf("            A        B        C        D        E\n");
+  for (t = 0; t <= 79; ++t) {
+    temp = S(5, A) + f(t, B, C, D) + E + W[t] + K(t);
+    E = D;
+    D = C;
+    C = S(30, B);
+    B = A;
+    A = temp;
+    printf("t = %2d: %08x %08x %08x %08x %08x\n", t, A, B, C, D, E);
+  }
+
+  H[0] += A;
+  H[1] += B;
+  H[2] += C;
+  H[3] += D;
+  H[4] += E;
+  printf("  sha1: %08x %08x %08x %08x %08x\n", H[0], H[1], H[2], H[3], H[4]);
 }
 
-void to_text(uint8_t* digest, char* text)
+// l: byte length
+uint8_t* sha1(uint8_t *message, uint64_t l, uint8_t* digest)
 {
-  int i;
-  for (i = 0; i < 20; ++i) {
-    text[2*i] = table[digest[i] >> 4];
-    text[2*i+1] = table[digest[i] & 0x0F];
+  uint64_t nblock, iblock;
+  uint8_t block[64];
+  int nremainder;
+
+  nblock = l / 64;
+  nremainder = l % 64; 
+
+  for (iblock = 0; iblock < nblock; ++iblock) {
+    memcpy(block, message, 64);
+    process_block(block, digest);
+    message += 64;
   }
+
+  memset(block, 0, 64);
+  memcpy(block, message, nremainder);
+  if (nremainder >= 56 ) { 
+  } else {
+    block[nremainder++] = 0x80;
+  }
+
+  int j;
+  l *= 8;
+  for (j = 0; j < 8; j++) {
+    block[56 + j] = (uint8_t) ((l >> (8 * (7 - j))) & 0xFF);
+  }
+  process_block(block, digest);
 }
+
+// void info(uint8_t* digest)
+// {
+//   int i;
+//   for (i = 0; i < 20; ++i)
+//     printf("%02x", digest[i]);
+//   printf("\n");
+// }
 
 int main(int argc, const char *argv[]) 
 {
   uint8_t digest[20];
-  char text[41];
   uint8_t* message = "abc";
-  sha1(message, 3, digest);
-  text[40] = '\0';
-  to_text(digest, text);
-  printf("message: %s\n", message);
-  // printf("digest: %s\n", digest);
-  printf("text: %s\n", text);
+  sha1(message, strlen(message), digest);
   return 0;
 }
