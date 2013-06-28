58c37d7c53fe330933412e5d9de2d4a230c5f7e3
Jingguo Yao <yaojingguo@gmail.com>
2013-06-28 21:44:21 +0800

    Update

--- stack_trace.c
+++ stack_trace.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// Use "gcc -g -rdynamic stack_trace.c -o stack_trace" to compile. For 
-// generating stack traces when C++ programs crash, refer to 
+// Use "gcc -g -rdynamic stack_trace.c" to compile. For generating stack traces 
+// when C++ programs crash, refer to 
 // http://stackoverflow.com/questions/77005/how-to-generate-a-stacktrace-when-my-gcc-c-app-crashes
 void bar() 
 { 
