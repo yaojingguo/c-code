c9a0dbe3b3b26b553cd3a6f00f04e0877b02b950
Jingguo Yao <yaojingguo@gmail.com>
2013-12-25 11:07:25 +0800

    Up

--- bithacks.c
+++ bithacks.c
@@ -1,4 +1,15 @@
+// http://graphics.stanford.edu/~seander/bithacks.html
 #include <stdio.h>
+#include <stdbool.h>
+
+bool parity(unsigned int v)
+{
+  v ^= v >> 16;
+  v ^= v >> 8;
+  v ^= v >> 4;
+  v &= 0xF;
+}
+
 
 int main(int argc, const char *argv[]) 
 {
