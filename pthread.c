8d6905db8b18b9b590189f8e3930c78978dbecef
Jingguo Yao <yaojingguo@gmail.com>
2014-02-15 15:45:40 +0800

    Up

--- pthread.c
+++ pthread.c
@@ -14,7 +14,7 @@ void* run(void* p)
   printf("unlocked\n");
 }
 
-int main(int argc, const char *argv[]) 
+void test_pthread_state()
 {
   pthread_t th; 
   assert(pthread_mutex_init(&lock, NULL) == 0);
@@ -22,5 +22,21 @@ int main(int argc, const char *argv[])
   assert(pthread_create(&th, NULL, run, NULL) == 0);
   sleep(100);
   assert(pthread_mutex_unlock(&lock) == 0);
+}
+
+
+// int pthread_cond_timedwait(pthread_cond_t *restrict cond,
+//     pthread_mutex_t *restrict mutex,
+//     const struct timespec *restrict abstime);
+// int pthread_cond_wait(pthread_cond_t *restrict cond,
+//     pthread_mutex_t *restrict mutex);
+// int pthread_cond_destroy(pthread_cond_t *cond);
+// int pthread_cond_init(pthread_cond_t *restrict cond,
+//     const pthread_condattr_t *restrict attr);
+// int pthread_cond_broadcast(pthread_cond_t *cond);
+// int pthread_cond_signal(pthread_cond_t *cond);
+
+int main(int argc, const char *argv[]) 
+{
   return 0;
 }
