ab017ca9c7bb95ef1b2bda26f38ca025b3dd9fe1
Jingguo Yao <yaojingguo@gmail.com>
2014-08-02 11:40:40 +0800

    Update

--- kernel/list_usage.c
+++ kernel/list_usage.c
@@ -22,11 +22,19 @@ void test2()
   list_add(&f1.list, &fox_list);
   struct fox f2 = { .weight = 2 };
   list_add_tail(&f2.list, &fox_list);
+  struct fox f0 = { .weight = 0 };
+  list_add(&f0.list, &fox_list);
 
   struct list_head* p;
   struct fox *f;
+  printf("Traversing...\n");
   list_for_each(p, &fox_list) {
     f = list_entry(p, struct fox, list);
+    printf("weight: %u\n", f->weight);
+  }
+
+  printf("Traversing reversely...\n");
+  list_for_each_entry_reverse(f, &fox_list, list) {
     printf("weight: %u\n", f->weight);
   }
 }
