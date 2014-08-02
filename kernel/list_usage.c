0da9f384005e6daa25f544fe6a6cd7eeca46e134
Jingguo Yao <yaojingguo@gmail.com>
2014-08-02 10:54:42 +0800

    Update

--- kernel/list_usage.c
+++ kernel/list_usage.c
@@ -1,20 +1,39 @@
-#include <linux/list.h>
 #include <stdio.h>
+#include "list.h"
 
 struct fox {
-  unsigned tail_length;
   unsigned weight;
-  bool     is_fantastic;
   struct list_head list;
 };
 
-int main(int argc, const char *argv[]) 
+void test1() 
 {
   struct fox red_fox = {
-    .tail_length = 40,
     .weight      = 6,
     .list = LIST_HEAD_INIT(red_fox.list),
   };
   printf("weight: %u\n", red_fox.weight);
+}
+
+void test2()
+{
+  static LIST_HEAD(fox_list);
+  struct fox f1 = { .weight = 1 };
+  list_add(&f1.list, &fox_list);
+  struct fox f2 = { .weight = 2 };
+  list_add_tail(&f2.list, &fox_list);
+
+  struct list_head* p;
+  struct fox *f;
+  list_for_each(p, &fox_list) {
+    f = list_entry(p, struct fox, list);
+    printf("weight: %u\n", f->weight);
+  }
+}
+
+int main(int argc, const char *argv[]) 
+{
+  test1();
+  test2();
   return 0;
 }
