4fe39749b2809209bbb0febebbb402fc9eb2742e
Jingguo Yao <yaojingguo@gmail.com>
2014-08-03 10:38:21 +0800

    Update

---
+++ kernel/hello.c
@@ -0,0 +1,26 @@
+#include <linux/init.h>
+#include <linux/module.h>
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int hello_init(void)
+{
+  printk(KERN_ALERT "Hello, world\n");
+  return 0;
+}
+
+static void hello_exit(void)
+{
+  printk(KERN_ALERT "Goodbye, cruel world\n");
+}
+
+/*module_ini(hello_init);*/
+/*module_exit(hello_exit);*/
+int init_module()
+{
+  return hello_init();
+}
+
+void cleanup_module()
+{
+  hello_exit();
+}
