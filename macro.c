e26348908c85b822b92d3def997ff3bfae9e0c8c
Jingguo Yao <yaojingguo@gmail.com>
2014-09-10 17:57:57 +0800

    Update

--- macro.c
+++ macro.c
@@ -3,8 +3,27 @@
 #define P(name) printf("%s\n", name);
 #define Q(name) printf("name: %s, %s\n", name, (name));
 
-int main()
+int test0()
 {
   P("ABC");
   Q("DEF");
+}
+
+#define abc def
+#define def func
+
+void func() 
+{
+  printf("hi, man\n");
+}
+
+int test1()
+{
+  abc();
+}
+
+int main()
+{
+  test0();
+  test1();
 }
