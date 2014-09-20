d691ccf27e4b6d14a701b3f2ad33b0b04663c788
Jingguo Yao <yaojingguo@gmail.com>
2014-09-20 11:27:54 +0800

    Update

--- kernel/modules/malloc_usage.c
+++ kernel/modules/malloc_usage.c
@@ -1,13 +1,15 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mm.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
+
 
 #define body(prefix) \
   do { \
     char *buf; \
-    buf = prefix##malloc(sizeof(int), GFP_KERNEL); \
+    MALLOC; \
     if (!buf) { \
       printk(KERN_ERR"kmalloc failed\n"); \
       return -ENOMEM; \
@@ -16,23 +18,34 @@ MODULE_LICENSE("GPL");
     buf[1] = 0x03; \
     buf[2] = 0x02; \
     buf[3] = 0x01; \
-    printf("int: 0x%08X\n", *((int *) buf)); \
+    printk("int: 0x%08X\n", *((int *) buf)); \
     prefix##free(buf); \
     return 0; \
-  } while (0);
+  } while (0)
 
-static int k() 
+static int k(void) 
 {
+#ifdef MALLOC
+#undef MALLOC
+#endif
+#define MALLOC do { buf = kmalloc(sizeof(int), GFP_KERNEL); } while (0)
   body(k);
+  return 0;
 }
 
-static int v()
+static int v(void)
 {
+#ifdef MALLOC
+#undef MALLOC
+#endif
+#define MALLOC do { buf = vmalloc(sizeof(int)); } while (0)
   body(v);
+  return 0;
 }
 
 int init_module(void)
 {
+  printk("malloc_usage init_module\n");
   k();
   v();
   return 0;
