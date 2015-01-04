55d2b34abec2c7cfe7371d298e06d5e9505abfd5
Jingguo Yao <yaojingguo@gmail.com>
2015-01-04 13:53:06 +0800

    Update

--- rfc/radix_64_convertion.c
+++ rfc/radix_64_convertion.c
@@ -6,17 +6,17 @@
 typedef long crc24;
 crc24 crc_octets(unsigned char *octets, size_t len)
 {
-	crc24 crc = CRC24_INIT;
-	int i;
-	while (len--) {
-		crc ^= (*octets++) << 16;
-		for (i = 0; i < 8; i++) {
-			crc <<= 1;
-			if (crc & 0x1000000)
-				crc ^= CRC24_POLY;
-		}
-	}
-	return crc & 0xFFFFFFL;
+  crc24 crc = CRC24_INIT;
+  int i;
+  while (len--) {
+    crc ^= (*octets++) << 16;
+    for (i = 0; i < 8; i++) {
+      crc <<= 1;
+      if (crc & 0x1000000)
+        crc ^= CRC24_POLY;
+    }
+  }
+  return crc & 0xFFFFFFL;
 }
 
 int main() 
