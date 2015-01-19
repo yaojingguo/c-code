f367f507da3918874ac80b2f1a06e9a15871f393
Jingguo Yao <yaojingguo@gmail.com>
2015-01-19 10:23:29 +0800

    Update

---
+++ socket/misc.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define LEN 1024
+
+void test_gethostname()
+{
+  char name[LEN];
+  if (gethostname(name, LEN) != 0) {
+    perror(NULL);
+    exit(EXIT_FAILURE);
+  }
+  printf("host name: %s\n", name);
+}
+
+
+int main(int argc, char *argv[])
+{
+  test_gethostname();
+  return 0;
+}
