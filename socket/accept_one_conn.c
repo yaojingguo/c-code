95e61c4c8740365412dbc3cdb9f94356c1a4f010
Jingguo Yao <yaojingguo@gmail.com>
2015-01-22 13:26:14 +0800

    Update

--- socket/accept_one_conn.c
+++ socket/accept_one_conn.c
@@ -15,7 +15,8 @@ int main(int argc, char *argv[]) {
 
   memset(&hints, 0, sizeof(hints));
   // Use IPv4 or IPv6 whichever
-  hints.ai_family = AF_UNSPEC;
+  // hints.ai_family = AF_UNSPEC;
+  hints.ai_family = AF_INET | AF_INET6;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
 
@@ -28,10 +29,12 @@ int main(int argc, char *argv[]) {
   listen(sockfd, BACKLOG);
 
   addr_size = sizeof(their_addr);
-  new_fd = accept(sockfd, 
-      (struct sockaddr*) &their_addr,
-      &addr_size);
-  write(new_fd, "abc", 3);
+  while (1) {
+    new_fd = accept(sockfd, 
+        (struct sockaddr*) &their_addr,
+        &addr_size);
+    write(new_fd, "abc\n", 4);
+  }
 
   close(new_fd);
   close(sockfd);
