c69852fd1e4b6dcbf4566f1fc3504936744cc7f9
Jingguo Yao <yaojingguo@gmail.com>
2015-01-23 16:04:45 +0800

    Update

--- scanf_c.c
+++ scanf_c.c
@@ -30,6 +30,24 @@ void test4()
   printf("%ld\n", strlen("中国"));
 }
 
+/*
+The unicode for 中 is 0x4E2D.
+
+
+UTF-8 encoding rules (For detaisl, refer to http://research.swtch.com/utf8)
+     Unicode code points		UTF-8 encoding (binary)
+          00-7F (7 bits)		0tuvwxyz
+     0080-07FF (11 bits)		110pqrst 10uvwxyz
+     0800-FFFF (16 bits)		1110jklm 10npqrst 10uvwxyz
+ 010000-10FFFF (21 bits)		11110efg 10hijklm 10npqrst 10uvwxyz
+
+ 0x4E2D is 11 bits. It is binary format is:
+
+      binary:      0100  111000  101101
+  UTF-8 bits:  1110    10      10
+       UTF-8:  111001001011100010101101  0xe4b8ad
+
+*/
 void test5()
 {
   char *c = "中";
