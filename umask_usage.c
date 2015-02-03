e7f56d09e0ca00c178718175a1636e15369eb693
Jingguo Yao <yaojingguo@gmail.com>
2015-02-03 16:37:42 +0800

    Update

--- umask_usage.c
+++ umask_usage.c
@@ -4,14 +4,31 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Code to show the effect of umask, the following link is a good reference:
 // http://askubuntu.com/questions/44542/what-is-umask-and-how-does-it-work
+
+#define LEN 1024
 
 void exit_failure()
 {
   perror(NULL);
   exit(EXIT_FAILURE);
+}
+
+void read_file(const char *pathname)
+{
+  int fd;
+  if ((fd = open(pathname, O_RDONLY, 0)) < 0)
+    exit_failure();
+  char buf[LEN];
+  memset(buf, 0, LEN);
+  if (read(fd, buf, LEN) < 0)
+    exit_failure();
+  printf("content: %s", buf);
+  if (close(fd) != 0)
+    exit_failure();
 }
 
 void create_file(const char *pathname, mode_t mode)
@@ -21,7 +38,7 @@ void create_file(const char *pathname, mode_t mode)
   if ((fd = creat(pathname, mode)) < 0)
     exit_failure();
 
-  if (write(fd, "A\n", 2) < 0)
+  if (write(fd, "B\n", 2) < 0)
     exit_failure();
 
   if (close(fd) != 0)
@@ -32,5 +49,6 @@ int main(int argc, char *argv[])
 {
   create_file("0666", 0666); 
   create_file("0777", 0777); 
+  // read_file("foo/one");
   return 0;
 }
