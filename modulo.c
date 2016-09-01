de38730d3d3d795b6fa848483d0434c64ece7d6e
Jingguo Yao <yaojingguo@gmail.com>
2016-09-01 16:10:41 +0800

    Update

--- modulo.c
+++ modulo.c
@@ -1,29 +1,45 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 
 int divide(int a, int b);
+int mod(int a, int b);
 
 int main()
 {
-  assert(divide(0, 5) == 0);
+    // divide
+    assert(divide(0, 5) == 0);
 
-  assert(divide(10, 5) == 2);
-  assert(divide(11, 5) == 2);
+    assert(divide(10, 5) == 2);
+    assert(divide(11, 5) == 2);
 
-  assert(divide(-10, -5) == 2);
-  assert(divide(-11, -5) == 2);
+    assert(divide(-10, -5) == 2);
+    assert(divide(-11, -5) == 2);
 
-  assert(divide(5, -3) == -2);
-  assert(divide(-5, 3) == -2);
+    assert(divide(5, -3) == -2);
+    assert(divide(-5, 3) == -2);
+
+    // mod
+    assert(mod(5, 3) == 2);
+    assert(mod(5, -3) == -1);
+    assert(mod(-5, 3) == 1);
+    assert(mod(-5, -3) == -2);
+}
+
+int mod(int a, int b) 
+{
+    int q = divide(a, b);
+    printf("q: %d\n", q);
+    return a - b * q;
 }
 
 int divide(int a, int b)
 {
-  if (a == 0 || b == 0) return a / b;
-  if (a > 0 && b > 0) return a / b;
-  if (a < 0 && b < 0) return a / b;
-  int quotient = a / b;
-  if (quotient * b > a)
-    quotient--;
-  return quotient;
+    if (a == 0 || b == 0) return a / b;
+    if (a > 0 && b > 0) return a / b;
+    if (a < 0 && b < 0) return a / b;
+    int quotient = a / b;
+    if (abs(quotient * b) < abs(a))
+        quotient--;
+    return quotient;
 }
