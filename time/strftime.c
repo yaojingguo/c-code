fb079a69fdd380fa867b66ba20e7243cc2750440
Jingguo Yao <yaojingguo@gmail.com>
2019-01-29 10:59:20 +0800

    update

--- time/strftime.c
+++ time/strftime.c
@@ -6,18 +6,20 @@ int main(int argc, char *argv[]) {
     time_t t;
     struct tm *tmp;
     char buf1[16];
-    char buf2[64];
+    char buf2[128];
 
     time(&t);
     tmp = localtime(&t);
 
-    if (strftime(buf1, 16, "time and date: %r, %a %b %d, %Y", tmp) == 0)
+    char* fmt = "time and date: %r, %a %b %d, %Y, %Z";
+
+    if (strftime(buf1, 16, fmt, tmp) == 0)
         printf("buffer length 16 is too small\n");
     else
         printf("%s\n", buf1);
 
-    if (strftime(buf2, 64, "time and date: %r, %a %b %d, %Y", tmp) == 0)
-        printf("buffer length 64 is too small\n");
+    if (strftime(buf2, 128, fmt, tmp) == 0)
+        printf("buffer length 128 is too small\n");
     else
         printf("%s\n", buf2);
 
