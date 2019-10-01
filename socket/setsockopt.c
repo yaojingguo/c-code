709c42a12a112a3c53759386772a9333f799b61c
Jingguo Yao <yaojingguo@gmail.com>
2019-10-01 22:05:08 +0800

    update

--- socket/setsockopt.c
+++ socket/setsockopt.c
@@ -9,53 +9,61 @@
 int main() {
     int sockfd;
 
-    socklen_t i;
+    socklen_t optlen;
     size_t len;
     size_t t1, t2;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    i = sizeof(len);
-    if (getsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &len, &i) < 0) {
+    optlen = sizeof(len);
+    if (getsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &len, &optlen) < 0) {
         perror(": getsockopt");
     }
     printf("receive buffer size = %ld\n", len);
 
-    if (getsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &len, &i) < 0) {
+    if (getsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &len, &optlen) < 0) {
         perror(": getsockopt");
     }
-    printf("send buffer size = %d\n", len);
+    printf("send buffer size = %ld\n", len);
 
-    if (getsockopt(sockfd, SOL_SOCKET, SO_RCVLOWAT, &len, &i) < 0) {
+    if (getsockopt(sockfd, SOL_SOCKET, SO_RCVLOWAT, &len, &optlen) < 0) {
         perror(": getsockopt");
     }
-    printf("rcv low watermark= %d\n", len);
+    printf("rcv low watermark= %ld\n", len);
 
-    if (getsockopt(sockfd, SOL_SOCKET, SO_SNDLOWAT, &len, &i) < 0) {
+    if (getsockopt(sockfd, SOL_SOCKET, SO_SNDLOWAT, &len, &optlen) < 0) {
         perror(": getsockopt");
     }
-    printf("snd low watermark = %d\n", len);
+    printf("snd low watermark = %ld\n", len);
+
+    /*struct timeval tv;*/
+    /*if (getsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &len, &tv) < 0) {*/
+    /*    perror(": getsockopt");*/
+    /*}*/
+    /*printf("tv_sec: %d\n", tv.tv_sec);*/
+    /*printf("tv_usec: %d\n", tv.tv_usec);*/
 
 
     t1 =1000000; t2 = sizeof(int);
     if (setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &t1, t2) < 0) {
         perror(": setsockopt");
     }
-    i = sizeof(len);
-    if (getsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &len, &i) < 0) {
+    optlen = sizeof(len);
+    if (getsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &len, &optlen) < 0) {
         perror(": getsockopt");
     }
-    printf("modified receive buffer size = %d\n", len);
+    printf("modified receive buffer size = %ld\n", len);
 
 
     t1 = 2; t2 = sizeof(int);
     if (setsockopt(sockfd, SOL_SOCKET, SO_RCVLOWAT, &t1, t2) < 0) {
         perror(": setsockopt");
     }
-    if (getsockopt(sockfd, SOL_SOCKET, SO_RCVLOWAT, &len, &i) < 0) {
+    if (getsockopt(sockfd, SOL_SOCKET, SO_RCVLOWAT, &len, &optlen) < 0) {
         perror(": getsockopt");
     }
-    printf("modified rcv low watermark= %d\n", len);
+    printf("modified rcv low watermark= %ld\n", len);
+
     
     int max_size;
     socklen_t size = sizeof(int);
