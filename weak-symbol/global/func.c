a35601269e4c169ea85d8c8ef1628a693c1bd046
Jingguo Yao <yaojingguo@gmail.com>
2020-12-05 18:51:10 +0800

    update

--- weak-symbol/global/func.c
+++ weak-symbol/global/func.c
@@ -2,20 +2,13 @@
 #include <pthread.h>
 
 int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
-                          void *(*start_routine) (void *), void *arg)
-{
-  return 0;
-}
+        void *(*start_routine) (void *), void *arg);
 
-int main()
-{
-	if (pthread_create)
-	{
-		printf("This is multi-thread version!\n");
-	}
-	else
-	{
-		printf("This is single-thread version!\n");
-	}
-	return 0;
+int main() {
+    if (pthread_create) {
+        printf("This is multi-thread version!\n");
+    } else {
+        printf("This is single-thread version!\n");
+    }
+    return 0;
 }
